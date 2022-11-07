8efeba7b92b18a3ab14873065cb3250dd830658a
ElonHoang <101619745+ElonHoang@users.noreply.github.com>
2022-11-07 13:31:59 +0700

    sdaf

--- HW5/src/main/java/com/aptech/hw5/repo/DB_Type.java
+++
@@ -1,5 +0,0 @@
-package com.aptech.hw5.repo;
-
-public enum DB_Type {
-SQL,SQLite
-}
--- HW5/src/main/java/com/aptech/hw5/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.aptech.hw5.service;
-
-public interface UserService {
-	public boolean checkLogin(String user, String pass);
-}
